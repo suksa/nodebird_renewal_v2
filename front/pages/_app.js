14178f8ec434771511f5424789dd386a8f4e8147
suksa <suksa_@naver.com>
2020-06-17 23:11:30 +0900

    3-1

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -1,6 +1,8 @@
 import PropTypes from 'prop-types'
-import 'antd/dist/antd.css'
 import Head from 'next/head'
+import 'antd/dist/antd.css'
+
+import wrapper from '../store/configureStore'
 
 const NodeBird = ({ Component }) => {
     return (
@@ -17,5 +19,4 @@ const NodeBird = ({ Component }) => {
 NodeBird.propTypes = {
     Component: PropTypes.elementType.isRequired
 }
-
-export default NodeBird
+export default wrapper.withRedux(NodeBird) //redux wrapper 사용시 provider 사용안함
